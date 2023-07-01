d1065137db1a105d33db5125ad7b713b0fbe2a2f
ricky-kiva <ricky.kiva@gmail.com>
2023-07-01 20:51:05 +0700

    add AddTask Activity Intent Test

--- app/src/androidTest/java/com/dicoding/todoapp/ui/list/TaskActivityTest.kt
+++ app/src/androidTest/java/com/dicoding/todoapp/ui/list/TaskActivityTest.kt
@@ -1,6 +1,27 @@
 package com.dicoding.todoapp.ui.list
 
-//TODO 16 : Write UI test to validate when user tap Add Task (+), the AddTaskActivity displayed
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+import com.dicoding.todoapp.R
+
+// XTODO 16 : Write UI test to validate when user tap Add Task (+), the AddTaskActivity displayed
 class TaskActivityTest {
+
+    @Suppress("DEPRECATION")
+    @Rule
+    @JvmField
+    val activityRule = ActivityTestRule(TaskActivity::class.java)
+
+    @Test
+    fun testAddTaskIntent() {
+        onView(withId(R.id.fab)).perform(click())
+        onView(withId(R.id.add_ed_title)).check(matches(isDisplayed()))
+    }
 
 }
