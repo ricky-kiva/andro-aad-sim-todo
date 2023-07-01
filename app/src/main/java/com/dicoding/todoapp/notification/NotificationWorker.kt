8c1089ae6dd45628cb879cfd501b29871c12d08c
ricky-kiva <ricky.kiva@gmail.com>
2023-07-01 20:50:46 +0700

    add WorkManager

--- app/src/main/java/com/dicoding/todoapp/notification/NotificationWorker.kt
+++ app/src/main/java/com/dicoding/todoapp/notification/NotificationWorker.kt
@@ -1,20 +1,27 @@
 package com.dicoding.todoapp.notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.core.app.TaskStackBuilder
+import androidx.preference.PreferenceManager
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.dicoding.todoapp.R
 import com.dicoding.todoapp.data.Task
+import com.dicoding.todoapp.data.TaskRepository
 import com.dicoding.todoapp.ui.detail.DetailTaskActivity
 import com.dicoding.todoapp.utils.NOTIFICATION_CHANNEL_ID
 import com.dicoding.todoapp.utils.TASK_ID
 
-class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
+class NotificationWorker(private val ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
 
-    private val channelName = inputData.getString(NOTIFICATION_CHANNEL_ID)
+    private val taskRepository = TaskRepository.getInstance(ctx)
+    private val channelName = inputData.getString(NOTIFICATION_CHANNEL_ID) ?: "notify-task"
 
     private fun getPendingIntent(task: Task): PendingIntent? {
         val intent = Intent(applicationContext, DetailTaskActivity::class.java).apply {
@@ -31,8 +38,37 @@ class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, p
     }
 
     override fun doWork(): Result {
-        //TODO 14 : If notification preference on, get nearest active task from repository and show notification with pending intent
+        // XTODO 14 : If notification preference on, get nearest active task from repository and show notification with pending intent
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        val notificationPreferenceOn = sharedPreferences.getBoolean(ctx.getString(R.string.pref_key_notify), false)
+
+        if (notificationPreferenceOn) {
+            val nearestActiveTask = taskRepository.getNearestActiveTask()
+            showNotification(nearestActiveTask)
+        }
         return Result.success()
+    }
+
+    private fun showNotification(task: Task) {
+
+        val notificationId = 1
+
+        val notificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notification: NotificationCompat.Builder = NotificationCompat.Builder(applicationContext, channelName)
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setContentTitle(task.title)
+            .setContentText(task.description)
+            .setContentIntent(getPendingIntent(task))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelName, channelName, NotificationManager.IMPORTANCE_HIGH)
+            notification.setChannelId(channelName)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(notificationId, notification.build())
     }
 
 }
--- app/src/main/java/com/dicoding/todoapp/setting/SettingsActivity.kt
+++ app/src/main/java/com/dicoding/todoapp/setting/SettingsActivity.kt
@@ -5,33 +5,67 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
+import androidx.work.*
 import com.dicoding.todoapp.R
+import com.dicoding.todoapp.notification.NotificationWorker
+import com.dicoding.todoapp.utils.NOTIFICATION_CHANNEL_ID
+import java.util.concurrent.TimeUnit
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var workManager: WorkManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
+
+        workManager = WorkManager.getInstance(this)
+
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
+                .replace(R.id.settings, SettingsFragment(workManager))
                 .commit()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
+    class SettingsFragment(private val workManager: WorkManager): PreferenceFragmentCompat() {
+
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
             val prefNotification = findPreference<SwitchPreference>(getString(R.string.pref_key_notify))
             prefNotification?.setOnPreferenceChangeListener { preference, newValue ->
                 val channelName = getString(R.string.notify_channel_name)
-                //TODO 13 : Schedule and cancel daily reminder using WorkManager with data channelName
+                // XTODO 13 : Schedule and cancel daily reminder using WorkManager with data channelName
+                if (newValue as Boolean) {
+                    scheduleDailyReminder(channelName)
+                } else {
+                    cancelDailyReminder()
+                }
                 true
             }
+        }
 
+        private fun scheduleDailyReminder(channelName: String) {
+
+            val inputData = workDataOf(NOTIFICATION_CHANNEL_ID to channelName)
+
+            val dailyReminderRequest = PeriodicWorkRequestBuilder<NotificationWorker>(1, TimeUnit.DAYS)
+                .setInputData(inputData)
+                .addTag(REMINDER_WORK_TAG)
+                .build()
+
+            workManager.enqueueUniquePeriodicWork(
+                REMINDER_WORK_TAG,
+                ExistingPeriodicWorkPolicy.REPLACE,
+                dailyReminderRequest
+            )
+        }
+
+        private fun cancelDailyReminder() {
+            workManager.cancelAllWorkByTag(REMINDER_WORK_TAG)
         }
 
         private fun updateTheme(mode: Int): Boolean {
@@ -39,5 +73,10 @@ class SettingsActivity : AppCompatActivity() {
             requireActivity().recreate()
             return true
         }
+
+        companion object {
+            private const val REMINDER_WORK_TAG = "daily_reminder"
+        }
+
     }
 }
